11e1d159e3e1e9d180e5eb50e6356925bdf21abe
anamika9693 <115655089+anamika9693@users.noreply.github.com>
2024-03-18 10:36:05 +0000

    datatypes

---
+++ 01_basics/01_var.js
@@ -0,0 +1,21 @@
+const accountId=6048101003320;
+let email="anamika52priya@gamil.com"; //let, scope problem get elminated
+var password="12345"
+city="bihar"
+
+// accountId=098879e582975;  not allowed:const var cant change
+
+email="ahgouaehg@gmail.com"
+password="q97509";
+city="nahoiajf";
+
+console.log(accountId);
+// console.log(email);
+// console.log(password);
+// console.log(city);
+
+console.table([email,password,city])
+
+/* pefer not to use var because if issue in block scope and functional scope*/
+let cus_name;
+console.log(cus_name);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+4)   //we r using node not browser
+
+/*less number of datatypes
+let name="anamika"
+let age=20
+*/
+
+//range of numbers is 2 to power 53
+//bigint
+//null =>standalone value
+//undefine=>value yet not assigned
+//symbols used in react to find uniqueness in components
+
+
+//object
+console.log(typeof null)  //output as object
+console.log(typeof undefined) //->undefined
+
