5b0b2cb0ccf2200512b17fc08876c44d60f9610e
anamika9693 <115655089+anamika9693@users.noreply.github.com>
2024-03-19 08:51:46 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+
+// console.log("2">1)    //"2" converted into num 
+// console.log("02">1)
+
+// console.log(null>0) //false
+// console.log(null==0) //false;
+// console.log(null>=0)// true;   null to num treated as 0 so null>=0 true  // sometime null convert into NaN
+
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined<0)  //all false
+
+
+// === strict check  , also check datatype
+
+// console.log("2"===2)   //false
+
