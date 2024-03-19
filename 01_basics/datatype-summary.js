94bc8d23225960e24f733556babe71948c8cf978
anamika9693 <115655089+anamika9693@users.noreply.github.com>
2024-03-19 14:42:59 +0000

    datatypeSummary

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -11,10 +11,10 @@
 const id=Symbol('123')   //symbol give a unique return type of type symbol
 const newId=Symbol('123')
 
-console.log(id==newId)  //false
+// console.log(id==newId)  //false
 
 const bigNum=25416523165841n
-console.log(typeof bigNum)
+// console.log(typeof bigNum)
 
 //Array
 const heros=["saktiman","krish","nagraj", "doga"];
@@ -30,7 +30,37 @@ const myFun=function(){
     console.log("Hello World")
 }
 
-console.log(typeof null)  //object
-console.log(typeof myFun)
+// console.log(typeof null)  //object
+// console.log(typeof myFun)  //return type is functional object
 
 //all non primitive datatype has return type as object
+
+
+
+//==========================================================================================
+
+
+
+//memory allocation
+//stack (primitive)->copy milti hai original value ki ,heap(non-primitive) ->reference milti hai to changes kuch kraa to direct original vlaue me change hoyegi
+
+
+// let name="anamika"
+// let another_name=name;
+// another_name="saloni"
+
+// console.log(name)
+// console.log(another_name)
+
+
+let userOne={
+    email:"anamika@gmail.com",
+    upi:"user@ybl"
+}
+
+// let userTwo=userOne
+// userTwo.email="wrwetj@gmail.com" //pass by refernece hota hai to dono me hi value change ho jaayegi
+// console.log(userOne.email)
+// console.log(userTwo.email)
+
+
