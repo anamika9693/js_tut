8a9f6e46e2cdbc85e260fc728906e8e8c8aa43c8
anamika9693 <115655089+anamika9693@users.noreply.github.com>
2024-03-20 09:32:47 +0000

    nums_math

---
+++ 01_basics/06_nums-math.js
@@ -0,0 +1,43 @@
+const score=100;
+// console.log(score)
+
+const balance= new Number(20903); //specifically var defined as a number then use new keyword
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))  //decimal up to 2
+
+
+const otherNum=23.8966
+// console.log(otherNum.toPrecision(1))  //return a string containing a number (give round off values)...careful while using (expeemtial me change kr de skta)
+
+
+const hundreds=10000000
+// console.log(hundreds.toLocaleString()) //adding commas
+// console.log(hundreds.toLocaleString('en-IN')) //adding commas like indian values
+
+
+
+
+
+// ====================Maths=============================
+// console.log(Math) //return obj
+
+
+// console.log(Math.abs(-34))
+// console.log(Math.round(-385.9885))
+// console.log(Math.ceil(4.001))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4,75,4,0.5,24))
+// console.log(Math.max(4,75,4,0.5,24))
+
+
+// console.log(Math.random())  //always come in between 01
+// console.log(Math.random()*10+1)   //1-9
+// console.log(Math.floor(Math.random()*10+1))
+
+
+const min=10;
+const max=20;
+// console.log(Math.floor(Math.random()*(max-min+1))+min) // range is b/w min and max //rem formulae 
+
